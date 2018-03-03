e0dd6e9507c3baea1e1e4ab420b7beca12ab6540
jin521 <jinlc220@126.com>
2018-03-03 17:21:09 +1100

    add association between Campaign and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303055506) do
+ActiveRecord::Schema.define(version: 20180303061848) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_campaigns_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
